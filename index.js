5322c929490bbf4597600d2861f3390604928ff5
Antonlih <84088882+Antonlih@users.noreply.github.com>
2021-12-03 13:50:29 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -10,11 +10,16 @@ const path = require('path')
 
 const PORT = process.env.PORT || 5000
 
+const IP = process.env.IP
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(express.static(path.resolve(__dirname, 'text')))
+app.use(express.static(path.resolve(__dirname, 'fonts')))
+app.use(express.static(path.resolve(__dirname, 'styles')))
 app.use(fileUpload({}))
 app.use('/api', router)
 app.use(errorHandler)
@@ -25,7 +30,7 @@ const start = async () => {
     try{
         await sequelize.authenticate()
         await sequelize.sync()
-        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+        app.listen(PORT, IP, () => console.log(`Server started on port ${PORT}, IP:${IP}`))
     } catch (e) {
         console.log(e)
     }
